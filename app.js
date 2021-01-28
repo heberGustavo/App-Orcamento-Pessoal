8f50eeedcd43b28b0d598b12a090a42d46edc228
Heber Gustavo <heber.gbarbosa@hotmail.com>
2021-01-28 13:22:53 -0300

    Pegando dados da View

---
+++ app.js
@@ -0,0 +1,15 @@
+function cadastrarDespesa(){
+    var ano = document.getElementById('ano');
+    var mes = document.getElementById('mes');
+    var dia = document.getElementById('dia');
+    var tipo = document.getElementById('tipo');
+    var descricao = document.getElementById('descricao');
+    var valor = document.getElementById('valor');
+
+    console.log(ano.value)
+    console.log(mes.value)
+    console.log(dia.value)
+    console.log(tipo.value)
+    console.log(descricao.value)
+    console.log(valor.value)
+}
